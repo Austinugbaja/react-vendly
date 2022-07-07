9e2b783d99a412108a28411c8b027319d4c01749
Augustine Ugbaja <augustineugbaja97@gmail.com>
2022-07-07 17:43:53 +0100

    made changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import CreateNewProduct from "./components/createproduct/CreateNewProduct";
 import EditProduct from "./components/createproduct/EditProduct";
 import Home from "./components/createproduct/Index";
-import SaveChanges from "./components/SaveChanges";
-import Welcome from "./components/Welcome";
+import ChangesCreated from "./components/popupModals/ChangesCreated";
+import DelProduct from "./components/popupModals/DelProduct";
+import ProductModal from "./components/popupModals/ProductModal";
+import SaveChanges from "./components/popupModals/SaveChanges";
+import Welcome from "./components/popupModals/Welcome";
+
 import "./styles/index.css";
 
 const App = () => {
@@ -16,6 +20,9 @@ const App = () => {
       </Route>
       <Route path="/welcome" element={<Welcome />} />
       <Route path="/savechanges" element={<SaveChanges />} />
+      <Route path="/changescreated" element={<ChangesCreated />} />
+      <Route path="/delproduct" element={<DelProduct />} />
+      <Route path="/productmodal" element={<ProductModal />} />
     </Routes>
   );
 };
--- src/components/popupModals/Welcome.js
+++ src/components/popupModals/Welcome.js
@@ -1,6 +1,6 @@
 import { Button } from "antd";
 import React from "react";
-import { dummyData } from "./data/dummyData";
+import { dummyData } from "../data/dummyData";
 
 const { ...data } = dummyData;
 
@@ -12,9 +12,12 @@ const { ...data } = dummyData;
 
 const Welcome = () => {
   return (
-    <div className="w-full flex items-center justify-center h-[100vh] text-center">
-      <div className="border-solid border-2 border-red-600" key={1}>
-        <p className="">{data[0]?.title}</p>
+    <div className="w-full flex items-center justify-center h-[100vh] text-center bg-gray-400 p-6">
+      <div
+        className="border-solid border-2 border-white p-6 px-16 bg-white rounded-md"
+        key={1}
+      >
+        <p className="text-lg font-semibold mt-5">{data[0]?.title}</p>
         <div>
           <img src={data[0]?.img} alt="" />
         </div>
@@ -22,10 +25,13 @@ const Welcome = () => {
         <div>{data[0]?.desc2}</div>
         <Button
           style={{
-            backgroundColor: "blue",
+            backgroundColor: "#0055D4",
             color: "white",
             borderRadius: "5px",
             border: "none",
+            marginTop: "2rem",
+            marginBottom: "2rem",
+            padding: "0 2rem",
           }}
         >
           CREATE PRODUCT
