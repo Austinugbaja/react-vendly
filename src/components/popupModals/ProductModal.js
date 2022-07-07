89d395f06ef0b7e102d0fcbb2db0d834b4375eda
Augustine Ugbaja <augustineugbaja97@gmail.com>
2022-07-07 17:53:17 +0100

    made changes

--- src/components/popupModals/ProductModal.js
+++ src/components/popupModals/ProductModal.js
@@ -4,7 +4,7 @@ const ProductModal = () => {
   return (
     <div className="w-full h-[100vh] flex items-center justify-center p-4 bg-gray-600 bg-transparent">
       <div
-        className="w-full text-center p-5 rounded-lg flex flex-col mx-96"
+        className="w-full text-center p-5 rounded-lg flex flex-col sm:mx-96 mx-4"
         style={{ backgroundColor: "#0055D4" }}
       >
         <div className="p-10 font-semibold text-white hover:bg-white hover:text-blue-600">
